6d8ab55b8f7731faf50881a04c485b98064cb387
YomalSmarakoon <yomal.2018471@iit.ac.lk>
2025-11-25 07:45:32 +0530

    Added exceptions

--- src/main/java/com/optimagrowth/organization/controller/OrganizationController.java
+++ src/main/java/com/optimagrowth/organization/controller/OrganizationController.java
@@ -3,6 +3,8 @@ package com.optimagrowth.organization.controller;
 import com.optimagrowth.organization.model.Organization;
 import com.optimagrowth.organization.service.OrganizationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,11 +18,12 @@ public class OrganizationController {
     private OrganizationService organizationService;
 
     @GetMapping("/{organizationId}")
-    public Organization getOrganization(@PathVariable("organizationId") String orgId) {
+    public ResponseEntity<Organization> getOrganization(@PathVariable("organizationId") String orgId) {
         Organization org = organizationService.getOrganizationById(orgId);
         if (org == null) {
-            throw new RuntimeException("Organization not found: " + orgId);
+//            throw new RuntimeException("Organization not found: " + orgId);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
-        return org;
+        return ResponseEntity.ok(org);
     }
 }
